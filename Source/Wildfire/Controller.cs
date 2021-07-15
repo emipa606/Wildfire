58b344d3467008823dba4df66a9cb3f746e9d0c3
emipa606 <emil.offermo@litet.net>
2021-07-15 13:36:31 +0200

    Mod updated for 1.3 and passed autotests

--- Source/Wildfire/Controller.cs
+++ Source/Wildfire/Controller.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Verse;
 
 namespace Wildfire
 {
-	// Token: 0x02000002 RID: 2
-	public class Controller : Mod
-	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-		public override string SettingsCategory()
-		{
-			return "WildFire.Name".Translate();
-		}
+    // Token: 0x02000002 RID: 2
+    public class Controller : Mod
+    {
+        // Token: 0x04000001 RID: 1
+        public static Settings Settings;
 
-		// Token: 0x06000002 RID: 2 RVA: 0x00002061 File Offset: 0x00000261
-		public override void DoSettingsWindowContents(Rect canvas)
-		{
-			Controller.Settings.DoWindowContents(canvas);
-		}
+        // Token: 0x06000003 RID: 3 RVA: 0x0000206E File Offset: 0x0000026E
+        public Controller(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<Settings>();
+        }
 
-		// Token: 0x06000003 RID: 3 RVA: 0x0000206E File Offset: 0x0000026E
-		public Controller(ModContentPack content) : base(content)
-		{
-			Controller.Settings = base.GetSettings<Settings>();
-		}
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public override string SettingsCategory()
+        {
+            return "WildFire.Name".Translate();
+        }
 
-		// Token: 0x04000001 RID: 1
-		public static Settings Settings;
-	}
+        // Token: 0x06000002 RID: 2 RVA: 0x00002061 File Offset: 0x00000261
+        public override void DoSettingsWindowContents(Rect canvas)
+        {
+            Settings.DoWindowContents(canvas);
+        }
+    }
 }
